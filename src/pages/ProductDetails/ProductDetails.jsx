859eac5c1c8fe240adac4f78d852b6ce5327b442
goel-adarsh <142222593+goel-adarsh@users.noreply.github.com>
2024-04-07 16:11:02 +0530

    feat(error handling): Error Handling for Product Details card

--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -2,12 +2,13 @@ import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 import './ProductDetails.css';
+import Error from '../../components/Error/Error';
 
 export default function ProductDetails() {
   let { productId } = useParams();
   const [isFetching, setIsFetching] = useState(false);
   const [productDetail, setproductDetail] = useState({});
-  // const [error, setError] = useState();
+  const [error, setError] = useState();
 
   useEffect(() => {
     async function fetchProducts() {
@@ -23,9 +24,9 @@ export default function ProductDetails() {
         console.log(responseData);
         setproductDetail(responseData);
       } catch (error) {
-        // setError({
-        //   message: error.message || 'Unable to load the products :(',
-        // });
+        setError({
+          message: 'Unable to load the Product Details :(',
+        });
       }
       setIsFetching(false);
     }
@@ -33,41 +34,51 @@ export default function ProductDetails() {
   }, []);
 
   return (
-    <div class="card">
-      <div class="imageBox">
-        <img src={productDetail.images?.[0]} alt={productDetail.title} />
-      </div>
-      <div class="details">
-        <div class="content">
-          <h2 className="title">{productDetail.title}</h2>
-          <span className="category">{productDetail.category} </span>{' '}
-          <span
-            style={{
-              textTransform: 'lowercase',
-              fontWeight: '200',
-              letterSpacing: '0em',
-            }}
-          >
-            {' '}
-            by
-          </span>
-          <h1 className="brand">{productDetail.brand}</h1>
-          <span className="rating">{productDetail.rating}⭐</span>
-          <p className="description">{productDetail.description}</p>
-          <h3 className="discounted-price">
-            ${' '}
-            {Math.round(
-              ((100 - Number(productDetail.discountPercentage)) / 100) *
-                productDetail.price
-            )}
-          </h3>
-          <h6 className="price">M.R.P.-$ {productDetail.price}</h6>
-          <h3 className="discounted-percentage">
-            -{productDetail.discountPercentage}%
-          </h3>
-          <button>Buy Now</button>
+    <>
+      {error && <Error title={' An error occurred!'} message={error.message} />}
+      {!error && isFetching && (
+        <div className="card" style={{ alignItems: 'center' }}>
+          <span className="card-loader"></span>
         </div>
-      </div>
-    </div>
+      )}
+      {!error && !isFetching && (
+        <div class="card">
+          <div class="imageBox">
+            <img src={productDetail.images?.[0]} alt={productDetail.title} />
+          </div>
+          <div class="details">
+            <div class="content">
+              <h2 className="title">{productDetail.title}</h2>
+              <span className="category">{productDetail.category} </span>{' '}
+              <span
+                style={{
+                  textTransform: 'lowercase',
+                  fontWeight: '200',
+                  letterSpacing: '0em',
+                }}
+              >
+                {' '}
+                by
+              </span>
+              <h1 className="brand">{productDetail.brand}</h1>
+              <span className="rating">{productDetail.rating}⭐</span>
+              <p className="description">{productDetail.description}</p>
+              <h3 className="discounted-price">
+                ${' '}
+                {Math.round(
+                  ((100 - Number(productDetail.discountPercentage)) / 100) *
+                    productDetail.price
+                )}
+              </h3>
+              <h6 className="price">M.R.P.-$ {productDetail.price}</h6>
+              <h3 className="discounted-percentage">
+                -{productDetail.discountPercentage}%
+              </h3>
+              <button>Buy Now</button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 }
